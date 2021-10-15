f2e5431bbf4bf9634efd66241a7443b6f4771f84
nhamilton1 <nickh3592@gmail.com>
2021-10-15 10:18:25 -0400

    test after login

--- api/server.test.js
+++ api/server.test.js
@@ -70,3 +70,17 @@ describe('[POST] /api/auth/login', () => {
     expect(decoded).toHaveProperty('iat')
   })
 })
+
+describe('[GET] /api/jokes after login', () => {
+  it('requests with valid token obtain list of jokes', async () => {
+    let res = await request(server).post('/api/auth/login').send({ username: 'bob', password: 'test123' })
+    res = await request(server).get('/api/jokes').set('Authorization', res.body.token)
+    expect(res.body).toHaveLength(3)
+    expect(res.body).toMatchObject([
+      { id: "0189hNRf2g", joke: "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later." },
+      { id: "08EQZ8EQukb", joke: "Did you hear about the guy whose whole left side was cut off? He's all right now." },
+      { id: "08xHQCdx5Ed", joke: "Why didn’t the skeleton cross the road? Because he had no guts." },
+    ])
+
+  })
+})
